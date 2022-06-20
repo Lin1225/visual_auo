889f4e6a61977a29ae349849c434252c845eb68f
Lin1225 <a3ie9981@gmail.com>
2022-06-20 18:33:16 +0800

    add the annotation

--- src/4.with_ICP/datasets/dataset_Class.py
+++ src/4.with_ICP/datasets/dataset_Class.py
@@ -30,90 +30,39 @@ class PoseDataset(data.Dataset):
         self.list_label = label
         self.list_bbox = bbox
         self.list_obj = [1]
-        self.list_rank = []
         self.noise_trans = noise_trans
         self.refine = refine
 
-        self.length = len(self.list_rgb)
-
+        # camera intrinsic matrix 
         self.cam_cx = 642.827137787509
         self.cam_cy = 365.5359193507853
         self.cam_fx = 614.628928745069
         self.cam_fy = 616.8066524222719
 
-        start2 = timeit.default_timer()
-        # self.xmap = np.zeros((720,1280),np.int64)
-        # self.ymap = np.zeros((720,1280),np.int64)
-        # self.xmap = np.array([[j for i in range(1280)] for j in range(720)])
-        # self.ymap = np.array([[i for i in range(1280)] for j in range(720)])
         grid = np.indices((720, 1280))
         self.xmap = grid[0]
         self.ymap = grid[1]
-        # print(self.xmap)
-        # print(self.ymap)
-        stop2 = timeit.default_timer()
-        # print('Timexxxx: ', stop2-start2) 
         self.num = num
         self.add_noise = add_noise
-        # self.trancolor = transforms.ColorJitter(0.2, 0.2, 0.2, 0.05)
-        #self.norm = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        
-        self.norm = transforms.Normalize(mean = [110.92494, 112.14303, 112.76024], std = [53.10094, 50.37343, 51.4505]) #(mean = [112.985016, 93.449036, 106.11367], std = [57.782948, 51.31353, 54.971416]) #(mean = [104.86711, 109.139305, 115.26725], std = [54.103874, 50.71502, 46.96569 ])
-        #self.norm = transforms.Normalize(mean = [0.411, 0.427, 0.452], std = [0.212, 0.198, 0.184 ])
         
 
+    def __getitem__(self):   
         
-        self.border_list = [-1, 40, 80, 120, 160, 200, 240, 280, 320, 360, 400, 440, 720, 520, 560, 600, 1280, 680]
-        self.num_pt_mesh_large =800
-        self.num_pt_mesh_small = 100
-        self.symmetry_obj_idx = []
+        depth = np.array(self.list_depth) # depth image
+        label = np.array(self.list_label) # bounding box 
 
-    def __getitem__(self, index):   
-        
-        img = np.array(self.list_rgb)
-        depth = np.array(self.list_depth)
-        label = np.array(self.list_label)
-        # print(label.shape)
-        #print(index)
-        obj = self.list_obj[index-1]
-        # print("depth:",len(depth.nonzero()[0]))
-
-        mask_depth = ma.getmaskarray(ma.masked_not_equal(depth, 0))
-        # print("mask_depth:",len(mask_depth.nonzero()[0]))
-
-        mask_label = ma.getmaskarray(ma.masked_equal(label, np.array(255)))
-        # print("mask_label:",len(mask_label.nonzero()[0]))
-
+        mask_depth = ma.getmaskarray(ma.masked_not_equal(depth, 0)) # remove nan in depth image
+        mask_label = ma.getmaskarray(ma.masked_equal(label, np.array(255))) # get mask where 255
         mask = mask_label * mask_depth
-        # print("mask:",len(mask.nonzero()[1]))
-        # print("mask shape: ",np.shape(mask))
 
-        if self.add_noise:
-            img = self.trancolor(img)
 
-        img = np.array(img)[:, :, :3]
-        img = np.transpose(img, (2, 0, 1))
-        img_masked = img
-
-        rmin, rmax, cmin, cmax = get_bbox(self.list_bbox)
-        #rmin = 85
-        #rmax = 245
-        #cmin = 343
-        #cmax = 543
-        img_masked = img_masked[:, rmin:rmax, cmin:cmax]
-        p_img = np.transpose(img_masked, (1, 2, 0))
-        # scipy.misc.imsave('./{0}_input.png'.format(index), p_img)
-        # import imageio
-        # imageio.imwrite('./{0}_input.png'.format(index), p_img)
-
-        
+        rmin, rmax, cmin, cmax = get_bbox(self.list_bbox) # transform bounding box format
         choose = mask[rmin:rmax, cmin:cmax].flatten().nonzero()[0]
-        if len(choose) == 0:
-            
+
+        if len(choose) == 0:            
             cc = torch.LongTensor([0])
             return(cc, cc, cc, cc)
 
-        #print("yaya")
         if len(choose) > self.num:
             c_mask = np.zeros(len(choose), dtype=int)
             c_mask[:self.num] = 1
@@ -127,61 +76,14 @@ class PoseDataset(data.Dataset):
         ymap_masked = self.ymap[rmin:rmax, cmin:cmax].flatten()[choose][:, np.newaxis].astype(np.float32)
         choose = np.array([choose])
         
-        # cam_scale = 1.0
-        # pt2 = depth_masked / cam_scale
-        # pt0 = (ymap_masked - self.cam_cx) * pt2 / self.cam_fx
-        # pt1 = (xmap_masked - self.cam_cy) * pt2 / self.cam_fy
-        # cloud = np.concatenate((pt0, pt1, pt2), axis=1)
-        # cloud = cloud/1000.0
         cam_scale = 1000.0
         pt2 = depth_masked / cam_scale
         pt0 = (ymap_masked - self.cam_cx) * pt2 / self.cam_fx
         pt1 = (xmap_masked - self.cam_cy) * pt2 / self.cam_fy
         cloud = np.concatenate((pt0, pt1, pt2), axis=1)
-        # print("cloud shape: ",np.shape(cloud))
-        
-        # import open3d as o3d
-        # pcd = o3d.geometry.PointCloud()
-        # pcd.points = o3d.utility.Vector3dVector(cloud)
-        # o3d.io.write_point_cloud("filename.pcd", pcd)
-
-        #fw = open('evaluation_result/{0}_cld.xyz'.format(index), 'w')
-        #for it in cloud:
-        #    fw.write('{0} {1} {2}\n'.format(it[0], it[1], it[2]))
-        #fw.close()
-
-        #fw = open('evaluation_result/{0}_model_points.xyz'.format(index), 'w')
-        #for it in model_points:
-        #    fw.write('{0} {1} {2}\n'.format(it[0], it[1], it[2]))
-        #fw.close()
-
-        #fw = open('evaluation_result/{0}_tar.xyz'.format(index), 'w')
-        #for it in target:
-        #    fw.write('{0} {1} {2}\n'.format(it[0], it[1], it[2]))
-        #fw.close()
-
-        #print(np.shape(cloud))
-        #print(np.shape(choose))
-        #print(np.shape(img_masked))
-        #print(self.objlist.index(obj))
-        
-        #cloud = np.expand_dims(cloud, axis=0)
-        #choose = np.expand_dims(choose, axis=0)
-        #img_masked = np.expand_dims(img_masked, axis=0)
-
         return cloud.astype(np.float32)
 
-    def __len__(self):
-        return self.length
-
-    def get_sym_list(self):
-        return self.symmetry_obj_idx
-
-    def get_num_points_mesh(self):
-        if self.refine:
-            return self.num_pt_mesh_large
-        else:
-            return self.num_pt_mesh_small
+    
 
 
 
@@ -190,7 +92,9 @@ img_width = 720
 img_length = 1280
 
 def get_bbox(bbox):
-    #bbx = [bbox[1], bbox[1] + bbox[3], bbox[0], bbox[0] + bbox[2]]
+    '''
+        modify bounding box range
+    '''
     bbx = [bbox[0], bbox[2], bbox[1], bbox[3]]
     if bbx[0] < 0:
         bbx[0] = 0
@@ -234,16 +138,3 @@ def get_bbox(bbox):
         cmin -= delt
     return rmin, rmax, cmin, cmax
 
-
-def ply_vtx(path):
-    f = open(path)
-    assert f.readline().strip() == "ply"
-    #f.readline()
-    f.readline()
-    N = int(f.readline().split()[-1])
-    while f.readline().strip() != "end_header":
-        continue
-    pts = []
-    for _ in range(N):
-        pts.append(np.float32(f.readline().split()[:3]))
-    return np.array(pts)
